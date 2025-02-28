a05c15e68715f96f6c7795c45e84a2509efa624f
Катерина Чакмазова <chakmazova06@gmail.com>
2025-03-01 00:23:15 +0200

    Add more functions and tests

--- src/reduce-right-prototype.js
+++ src/reduce-right-prototype.js
@@ -23,6 +23,72 @@ function MyArray (...args) {
       }
       return this.length;
     }; 
+    this.pop = function(){
+      if(this.length === 0){
+       return;
+      }
+      const lastItem = this[this.length-1];
+      delete this[--this.length];
+      return lastItem;
+     };
+   
+     this.forEach = function(callback){
+        for(let i = 0; i < this.length; i++){
+         callback(this[i], i, this)
+        }
+     };
+     this.some = function(callback){
+       for(let i = 0; i < this.length; i++){
+         if(callback(this[i], i, this)){
+           return true;
+         }
+         return false;
+     }
+   };
+   this.every = function (callback) {
+     for(let i = 0; i < this.length; i++){
+       if(!callback(this[i], i, this)){
+         return false;
+   }
+     return true;
+   }
+   };
+   this.map = function(callback){
+     const result = new MyArray();
+     for (let i = 0; i < this.length; i++){
+       result.push(callback(this[i], i, this));
+     }
+     return result;
+   };
+   this.concat = function(...args){
+     const res = this;
+     for (let i = 0; i < args.length; i++){
+       if (Array.isArray(args[i])){
+         res.push(...args[i]);
+       }
+       else if (MyArray.isMyArray(args[i])){
+         for(let j = 0; j < args[i].length; j++){
+           res.push(args[i][j]);
+         }
+       }
+       else {
+         res.push(args[i]);
+       }
+     }
+     return res;
+   };
+   this.reverse = function(){
+     const lengthMinusOne = this.length - 1;
+     for(let i = 0; i < Math.floor(this.length / 2); i++){
+       // temp = this[i];
+       // this[i]= this[lengthMinusOne - i];
+       // this[lengthMinusOne - i] = temp;
+       [this[i], this[lengthMinusOne - i]] =[this[lengthMinusOne - i], this[i]]
+   
+   
+     }
+     return this;
+   }
     this.reduceRight = function(callback, initialValue){
       if(this.length === 0 && initialValue === undefined){
         throw new TypeError('Array is empty')
@@ -46,9 +112,10 @@ function MyArray (...args) {
   };
 }
 const myArr1 = new MyArray(100, 500, 900, 1000);
-
-// debugger
+const myArr = new MyArray();
 console.log(myArr1.reduceRight((a, b) => a - b, 1000)); 
 console.log(myArr1.reduceRight((a, b) => a + b, 100));
 console.log(myArr1.reduceRight((a, b) => a + b));
-console.log(myArr1.reduceRight((a, b) => a - b))
+console.log(myArr1.reduceRight((a, b) => a - b));
+// console.log(myArr.reduceRight((a, b) => a - b));
+console.log(myArr.reduceRight((a, b) => a - b, 100));
